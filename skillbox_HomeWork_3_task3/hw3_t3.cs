46eab82d9aaa41042e173080258254332de35a80
max <mxbrkn@proton.me>
2022-06-02 00:28:21 +0300

    done

---
+++ skillbox_HomeWork_3_task3/hw3_t3.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+int i = 2;
+bool simpleNumber = true;
+
+while (i < number - 1)
+{
+    if (number % i == 0)
+    {
+        simpleNumber = false;
+        break;
+    }
+    i++;
+}
+
+WriteLine(simpleNumber?"Число является простым":"Число не является простым");
+
+ReadKey();
