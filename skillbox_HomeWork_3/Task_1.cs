b485284a17075eadfebe9d8e4edba2faa94885b7
burkin <burkin@DESKTOP-CKSIPLE>
2022-06-01 22:12:28 +0300

    Добавьте файлы проекта.

---
+++ skillbox_HomeWork_3/Task_1.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите число: ");
+
+//Искренне надеемся, что пользователь введет именно число
+//Исключения будем обрабатывать когда-то потом
+int number = int.Parse(Console.ReadLine());
+
+//Будем конструировать слово в зависимости от условий
+string resultWord = "четным";
+
+if (number%2 > 0)
+{
+    resultWord = "не" + resultWord;
+}
+
+//Сообщаем результат
+Console.WriteLine($"Число является {resultWord}");
+Console.ReadKey();
---
+++ skillbox_HomeWork_3/Task_2.cs
@@ -0,0 +1,73 @@
+﻿Console.WriteLine("Добро пожаловать в Black Jack!!! \n \n Введите количество ваших карт:");
+int Number = int.Parse(Console.ReadLine());
+
+int Sum = 0;
+int CardNumber = 0;
+int intvalue;
+
+while (CardNumber > Number-1)
+{
+    Console.WriteLine("Введите номинал вашей следующей карты");
+    string strvalue = Console.ReadLine();
+
+    switch (strvalue)
+    {
+        case "1": 
+            intvalue = 1;
+            break;
+        case "2":
+            intvalue = 2;
+            break;
+        case "3":
+            intvalue = 3;
+            break;
+        case "4":
+            intvalue = 4;
+            break;
+        case "5":
+            intvalue = 5;
+            break;
+        case "6":
+            intvalue = 6;
+            break;
+        case "7":
+            intvalue = 7;
+            break;
+        case "8":
+            intvalue = 8;
+            break;
+        case "9":
+            intvalue = 9;
+            break;
+        case "10":
+            intvalue = 10;
+            break;
+        case "J":
+            intvalue = 10;
+            break;
+        case "Q":
+            intvalue = 10;
+            break;
+        case "K":
+            intvalue = 10;
+            break;
+        case "T":
+            intvalue = 10;
+            break;
+
+        default:
+            intvalue = 0;   
+            break;
+    }
+
+    Sum += intvalue;
+    CardNumber++;
+
+
+
+
+
+}
+
+Console.WriteLine($"Сумма ваших карт {Sum}");
+Console.ReadKey();
